fac0d07a7d91b3d9fe81ac7401ba6c1c2e3dc0e7
cheresanford <61995917+cheresanford@users.noreply.github.com>
2024-08-01 02:00:00 -0300

    Commit inicial

---
+++ rest-with-spring-boot-carlos/src/test/java/com/example/restwithspringbootcarlos/RestWithSpringBootCarlosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restwithspringbootcarlos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootCarlosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
